45a3dcff2da60359608d804075f283021d4ecdaf
whannan7 <whannan18@gmail.com>
2026-02-13 15:18:45 +0000

    commit

---
+++ print_ram.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+extern unsigned char ram[];
+
+extern void fill_ram(void);
+int main()
+
+{
+    fill_ram();
+    printf("RAM contents from 50H to 58H: \n");
+
+    for(int i = 0x50; i <= 0x58; i++)
+    { printf("%02X ", ram[i]);}
+
+    printf("\n");
+
+    return 0;
+}
